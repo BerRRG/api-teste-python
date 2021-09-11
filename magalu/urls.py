2d12d18b867ee4b6662b772bd8b1cec7302c3b77
bgenerozo <bernardo.generozo@zoly.com.br>
2021-09-11 17:14:57 -0300

    Adicionado urls de token

--- magalu/urls.py
+++ magalu/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from products.api import viewsets as productsviewsets
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 route = routers.DefaultRouter()
 
@@ -9,6 +10,8 @@ route.register('api/product', productsviewsets.ProductsViewSet)
 route.register('api/customer', productsviewsets.CustomersViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(route.urls))
+	path('token/', TokenObtainPairView.as_view()),
+	path('token/refresh/', TokenRefreshView.as_view()),
+	path('admin/', admin.site.urls),
+	path('', include(route.urls))
 ]
