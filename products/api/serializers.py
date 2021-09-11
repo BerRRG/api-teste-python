2d54318b55ea9beb8f0c2fd605ad37e015034c7c
bgenerozo <bernardo.generozo@zoly.com.br>
2021-09-11 16:00:15 -0300

    Corrigido serializers para remover metodo desnecessario

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -10,10 +10,3 @@ class CustomersSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = models.Customers
 		fields = ('id', 'email', 'name', 'favorites')
-
-		def validate_name(self, value):
-			# print(data)
-			# sys.exit()
-			if 'django' not in value.lower():
-				raise serializers.ValidationError("Blog post is not about Django")
-			return value
