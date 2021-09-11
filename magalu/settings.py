fd23ffb659d86f8a78c4812d166d9965b69b7b6a
bgenerozo <bernardo.generozo@zoly.com.br>
2021-09-11 15:57:35 -0300

    Adicionado configuração do banco de dados mysql

--- magalu/settings.py
+++ magalu/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'magalu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'api_python_django',
+        'USER': 'user_api_python',
+        'PASSWORD': '<SENHA>',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
